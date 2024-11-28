5c0d1556c6985a491f7152dd4a0ba5628edf3d5e
Victor Velazquez Cid <76626605+vvelc@users.noreply.github.com>
2024-11-28 19:46:44 -0400

    add icon and netlify support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
+import netlify from "@astrojs/netlify";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress(),]
+  integrations: [tailwind(), compress(), icon({
+    include: {
+      ic: ['*']
+    }
+  })],
+  output: "server",
+  adapter: netlify()
 });
